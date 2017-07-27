c4e2583d8c4c5abb2dc25629369e9f87bf8fbda6
JThink <leviqian@sina.com>
2017-07-27 15:19:56 +0800

    update jdk from 1.7 to 1.8

--- skyeye-data/skyeye-data-jpa/src/main/java/com/jthink/skyeye/data/jpa/configuration/JpaConfiguration.java
+++ skyeye-data/skyeye-data-jpa/src/main/java/com/jthink/skyeye/data/jpa/configuration/JpaConfiguration.java
@@ -1,6 +1,6 @@
 package com.jthink.skyeye.data.jpa.configuration;
 
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
