90f8a1596777b99ea55bc2d4cf59906fce7f45c1
qianjc <qianjc@unionpaysmart.com>
2017-11-24 14:50:50 +0800

    修改image构建为传递参数

--- skyeye-alarm/image/build.sh
+++ skyeye-alarm/image/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 version=$1
+branch=$2
 cp ../target/distributions/*.tar .
-sudo docker build -t 192.168.88.73:8888/skyeye/skyeye-alarm:$version .
+sudo docker build -t --build-arg branch=$branch --build-arg version=$version 192.168.88.73:8888/skyeye/skyeye-alarm:$version .
 sudo docker push 192.168.88.73:8888/skyeye/skyeye-alarm:$version
--- skyeye-alarm/image/run.sh
+++ skyeye-alarm/image/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+version=$1
+branch=$2
+
 source /etc/profile.d/java.sh
 
 cd /opt/jthink/jthink-config
-git pull -r origin master
-cd /home/deploy/skyeye-alarm-1.3.0
+git pull -r origin $branch
+cd /home/deploy/skyeye-alarm-$version
 nohup bin/skyeye-alarm &
 tail -f /opt/jthink/jthink-config/skyeye/alarm/alarm.properties
